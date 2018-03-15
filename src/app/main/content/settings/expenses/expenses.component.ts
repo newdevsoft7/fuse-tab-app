beafa88ff98cb0715d504f0d4314e105d07d4044
Newdevsoft7 <newdevsoft7@gmail.com>
2018-03-15 23:32:58 +1000

    Settings - Expenses

--- src/app/main/content/settings/expenses/expenses.component.ts
+++ src/app/main/content/settings/expenses/expenses.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+    Component, OnInit, Input,
+    Output, EventEmitter
+} from '@angular/core';
+
+import { MatSlideToggleChange, MatSelectChange } from '@angular/material';
+import { ToastrService } from 'ngx-toastr';
+import * as _ from 'lodash';
+
+import { SettingsService } from '../settings.service';
+
+enum Setting {
+    expenses_enable = 27,
+    expenses_staff = 28,
+    expense_receipt_required = 29
+}
 
 @Component({
     selector: 'app-settings-expenses',
@@ -7,12 +22,38 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SettingsExpensesComponent implements OnInit {
 
-	@Input() settings = [];
-	@Input() options = [];
+    @Input() settings = [];
+    @Input() options = [];
 
-    constructor() { }
+    @Output() settingsChange = new EventEmitter();
+    
+    readonly Setting = Setting;
+
+    constructor(
+        private settingsService: SettingsService,
+        private toastr: ToastrService
+    ) { }
 
     ngOnInit() {
     }
+    
+    value(id: Setting) {
+        if (_.isEmpty(this.settings)) return;
+        const value = _.find(this.settings, ['id', id]);
+        return _.toInteger(value.value) === 0 ? false : true;
+    }
+
+    onChange(id: Setting, event: MatSlideToggleChange) {
+        if (_.isEmpty(this.settings)) return;
+
+        const setting = _.find(this.settings, ['id', id]);
+        const value = event.checked ? 1 : 0;
+        this.settingsService.setSetting(id, value).subscribe(res => {
+            setting.value = value;
+            this.settingsChange.next(this.settings);
+            this.toastr.success(res.message);
+        });
+    }
+
 
 }
