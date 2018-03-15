ae174ba6a2fc3920f20d05a62ffb68f575b3087d
Newdevsoft7 <newdevsoft7@gmail.com>
2018-03-16 01:07:01 +1000

    Settings - Xero

--- src/app/main/content/settings/xero/xero.component.ts
+++ src/app/main/content/settings/xero/xero.component.ts
@@ -9,6 +9,13 @@ import * as _ from 'lodash';
 
 import { SettingsService } from '../settings.service';
 
+enum Setting {
+    xero_enable = 97,
+    xero_client_invoice = 98,
+    xero_staff_invoice = 99,
+    xero_payroll = 100
+}
+
 @Component({
     selector: 'app-settings-xero',
     templateUrl: './xero.component.html',
@@ -21,9 +28,32 @@ export class SettingsXeroComponent implements OnInit {
 
     @Output() settingsChange = new EventEmitter();
     
-    constructor() { }
+    readonly Setting = Setting;
+
+    constructor(
+        private settingsService: SettingsService,
+        private toastr: ToastrService
+    ) { }
 
     ngOnInit() {
+    }
+    
+    value(id: Setting) {
+        if (_.isEmpty(this.settings)) return;
+        const value = _.find(this.settings, ['id', id]);
+        return _.toInteger(value.value) === 0 ? false : true;
+    }
+
+    onChange(id: Setting, event: MatSlideToggleChange) {
+        if (_.isEmpty(this.settings)) return;
+
+        const setting = _.find(this.settings, ['id', id]);
+        const value = event.checked ? 1 : 0;
+        this.settingsService.setSetting(id, value).subscribe(res => {
+            setting.value = value;
+            this.settingsChange.next(this.settings);
+            this.toastr.success(res.message);
+        });
     }
 
 }
