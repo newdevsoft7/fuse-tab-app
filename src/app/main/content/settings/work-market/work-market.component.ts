2a34f3b4c17597e1d8e85a571588b9914ea76eaf
Newdevsoft7 <newdevsoft7@gmail.com>
2018-03-16 01:17:11 +1000

    Settings - Work Market

--- src/app/main/content/settings/work-market/work-market.component.ts
+++ src/app/main/content/settings/work-market/work-market.component.ts
@@ -9,6 +9,10 @@ import * as _ from 'lodash';
 
 import { SettingsService } from '../settings.service';
 
+enum Setting {
+    work_market_enable = 96
+}
+
 @Component({
     selector: 'app-settings-work-market',
     templateUrl: './work-market.component.html',
@@ -20,10 +24,33 @@ export class SettingsWorkMarketComponent implements OnInit {
     @Input() options = [];
 
     @Output() settingsChange = new EventEmitter();
+    
+    readonly Setting = Setting;
 
-    constructor() { }
+    constructor(
+        private settingsService: SettingsService,
+        private toastr: ToastrService
+    ) { }
 
     ngOnInit() {
+    }
+    
+    value(id: Setting) {
+        if (_.isEmpty(this.settings)) return;
+        const value = _.find(this.settings, ['id', id]);
+        return _.toInteger(value.value) === 0 ? false : true;
+    }
+
+    onChange(id: Setting, event: MatSlideToggleChange) {
+        if (_.isEmpty(this.settings)) return;
+
+        const setting = _.find(this.settings, ['id', id]);
+        const value = event.checked ? 1 : 0;
+        this.settingsService.setSetting(id, value).subscribe(res => {
+            setting.value = value;
+            this.settingsChange.next(this.settings);
+            this.toastr.success(res.message);
+        });
     }
 
 }
