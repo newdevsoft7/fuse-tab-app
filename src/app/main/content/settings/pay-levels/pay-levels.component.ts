3697934c594894d2748507628c598cf177606abb
Newdevsoft7 <newdevsoft7@gmail.com>
2018-03-17 08:04:54 +1000

    Settings - Pay Level [WIP]

--- src/app/main/content/settings/pay-levels/pay-levels.component.ts
+++ src/app/main/content/settings/pay-levels/pay-levels.component.ts
@@ -1,18 +1,112 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+    Component, OnInit, Input,
+    ViewEncapsulation, SimpleChanges,
+    OnChanges, Output, EventEmitter,
+    ViewChild, OnDestroy
+} from '@angular/core';
+
+import { FormControl, Validators } from '@angular/forms'
+
+import { Observable } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
+
+import { MatSlideToggleChange, MatSelectChange } from '@angular/material';
+import { ToastrService } from 'ngx-toastr';
+import * as _ from 'lodash';
+
+import { SettingsService } from '../settings.service';
+
+enum Setting {
+    paylvl_enable = 35
+}
+
 
 @Component({
     selector: 'app-settings-pay-levels',
     templateUrl: './pay-levels.component.html',
-    styleUrls: ['./pay-levels.component.scss']
+    styleUrls: ['./pay-levels.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class SettingsPayLevelsComponent implements OnInit {
 
-	@Input() settings = [];
-	@Input() options = [];
+    _settings = [];
 
-    constructor() { }
+    @Input('settings')
+    set settings(settings) {
+        this._settings = settings;
+    }
+
+    get settings() {
+        return this._settings;
+    }
+
+    @Input() options = [];
+
+    @Output() settingsChange = new EventEmitter();
+
+    readonly Setting = Setting;
+
+    items: any = {}; // All Settings
+
+    // Slide Togglable Items
+    checkableItems = [
+        Setting.paylvl_enable
+    ];
+
+    // Number Items
+    numberItems = [
+    ];
+
+    componentDestroyed = new Subject();
+
+    constructor(
+        private settingsService: SettingsService,
+        private toastr: ToastrService
+    ) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+
+        if (changes.settings || changes.options) {
+            
+            const keys = Object.keys(this.Setting).filter(v => _.isNumber(_.toNumber(v))) as string[];
+
+            _.forEach(keys, (v) => {
+                const item = _.find(this.settings, ['id', _.toNumber(v)]);
+                if (!_.isUndefined(item)) {
+                    if (this.checkableItems.includes(item.id)) { // Slide Togglable Items
+                        this.items = { ...this.items, [item.id]: _.toInteger(item.value) === 0 ? false : true };
+                    } else if (this.numberItems.includes(item.id)) { // Number Fields
+
+                    } else { // Text Fields
+                        this.items = { ...this.items, [item.id]: item.value };
+                    }
+                }
+            });
+        }
+
+    }
 
     ngOnInit() {
+    }
+
+    onChange(id: Setting, event: MatSlideToggleChange | MatSelectChange | string) {
+        if (_.isEmpty(this.settings)) return;
+
+        const setting = _.find(this.settings, ['id', id]);
+        let value;
+
+        if (event instanceof MatSlideToggleChange) { // Slide Toggle
+            value = event.checked ? 1 : 0;
+        } else if (event instanceof MatSelectChange) { // Select Box
+            value = event.value;
+        } else { // Input Text
+            value = event;
+        }
+        this.settingsService.setSetting(id, value).subscribe(res => {
+            setting.value = value;
+            this.settingsChange.next(this.settings);
+            this.toastr.success(res.message);
+        });
     }
 
 }
