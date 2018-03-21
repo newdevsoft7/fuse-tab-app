fc8683ad682b9aeeabdf6bdf7ed9a4eb9b01c5f2
Newdevsoft7 <newdevsoft7@gmail.com>
2018-03-22 06:46:05 +1000

    Registration - Step 0

---
+++ src/app/main/content/authentication/register/register.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
+
+const BASE_URL = `${environment.apiUrl}`;
+
+@Injectable()
+export class RegisterService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    /**
+     * Register in first step
+     * @param params
+     */
+    register(params): Observable<any> {
+        const url = `${BASE_URL}/auth/register`;
+        return this.http.post(url, params)
+            .catch(this.handleError);
+    }
+
+    /**
+     * Register in any steps except first step
+     * @param step
+     * @param params
+     */
+    registerByStep(step: string, params: any): Observable<any> {
+        const url = `${BASE_URL}/register`;
+        return this.http.post(url, { step, ...params })
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response | any) {
+        return Observable.throw(error);
+    }
+}
--- src/app/main/content/authentication/register/steps/0/register-step0.component.ts
+++ src/app/main/content/authentication/register/steps/0/register-step0.component.ts
@@ -1,10 +1,58 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register-step0',
-  templateUrl: './register-step0.component.html',
-  styleUrls: ['./register-step0.component.scss']
+    selector: 'app-register-step0',
+    templateUrl: './register-step0.component.html',
+    styleUrls: ['./register-step0.component.scss']
 })
-export class RegisterStep0Component {
+export class RegisterStep0Component implements OnInit {
 
+    form: FormGroup;
+    formErrors: any;
+
+    constructor(
+        private formBuilder: FormBuilder
+    ) {
+        this.formErrors = {
+            fname   : {},
+            lname   : {},
+            email   : {},
+            password: {},
+            mob     : {}
+        };
+    }
+
+    ngOnInit() {
+
+        this.form = this.formBuilder.group({
+            fname   : ['', Validators.required],
+            lname   : ['', Validators.required],
+            email   : ['', Validators.required],
+            password: ['', Validators.required],
+            mob     : ['', Validators.required]
+        });
+
+        this.form.valueChanges.subscribe(() => {
+            this.onFormValuesChanged();
+        });
+    }
+
+    onFormValuesChanged() {
+        for (const field in this.formErrors) {
+            if (!this.formErrors.hasOwnProperty(field)) {
+                continue;
+            }
+
+            // Clear previous errors
+            this.formErrors[field] = {};
+
+            // Get the control
+            const control = this.form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                this.formErrors[field] = control.errors;
+            }
+        }
+    }
 }
