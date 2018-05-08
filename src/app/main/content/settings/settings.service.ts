b26ca2707a60a123e389795515d29f22b2d4c076
jeremy <jeremyj@jj-projects.com>
2018-05-08 15:41:57 +0200

    moved change password to user profile settings

--- src/app/main/content/settings/settings.service.ts
+++ src/app/main/content/settings/settings.service.ts
@@ -238,12 +238,6 @@ export class SettingsService {
             .catch(this.handleError);
     }
 
-    changePassword(userId: number, password: string): Observable<any> {
-        const url = `${BASE_URL}/user/${userId}/password`;
-        return this.http.put(url, { password })
-            .catch(this.handleError);
-    }
-
     private handleError(error: Response | any) {
         return Observable.throw(error);
     }
