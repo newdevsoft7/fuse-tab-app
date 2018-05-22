7970f4f5fa93e78e42064fc057ceaeb4da6a4d29
Idris Adamjy <adamjyidris@gmail.com>
2018-05-22 21:44:21 +0200

    Fixed registration route guard

--- src/app/shared/guards/registration-guard.service.ts
+++ src/app/shared/guards/registration-guard.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, RouterStateSnapshot, Router } from "@angular/router";
 import { AppSettingService } from "../services/app-setting.service";
+import { AuthenticationService } from "../services/authentication.service";
 
 @Injectable()
 export class RegistrationGuardService implements CanActivate {
 
   constructor(
     private appSetting: AppSettingService,
+    private authService: AuthenticationService,
     private router: Router) {}
 
   canActivate(route, state: RouterStateSnapshot): boolean {
-    if (this.appSetting.baseData.registration_enable !== '1') {
+    if (!this.authService.isAuthorized() && this.appSetting.baseData.registration_enable !== '1') {
       this.router.navigate(['/login']);
       return false;
     }
