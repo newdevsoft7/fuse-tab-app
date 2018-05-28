710cbb3333e858410ddca2c258806d7931a91fde
Newdevsoft7 <newdevsoft7@gmail.com>
2018-05-29 01:27:43 +1000

    Fix complete modal

--- src/app/main/content/schedule/shift/staff-shift/info/dialogs/complete-dialog/complete-dialog.component.ts
+++ src/app/main/content/schedule/shift/staff-shift/info/dialogs/complete-dialog/complete-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { TokenStorage } from '../../../../../../../../shared/services/token-storage.service';
 
 @Component({
     selector: 'app-staff-shift-complete-dialog',
@@ -10,11 +11,15 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class StaffShiftCompleteDialogComponent implements OnInit {
 
     notes: string;
+    settings: any;
 
     constructor(
         public dialogRef: MatDialogRef<StaffShiftCompleteDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) private data: any
-    ) { }
+        @Inject(MAT_DIALOG_DATA) private data: any,
+        private tokenStorage: TokenStorage
+    ) {
+        this.settings = this.tokenStorage.getSettings();
+    }
 
     ngOnInit() {
     }
