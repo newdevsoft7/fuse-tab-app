5baefcb21e969674cb027be3629914e2a437068a
Idris Adamjy <adamjyidris@gmail.com>
2018-08-03 21:54:15 +0200

    Implement showcase SSR page

---
+++ src/showcase.php
@@ -0,0 +1,72 @@
+<?php
+
+$domain = $_SERVER['HTTP_HOST'];
+$user_id = $_GET['user_id'];
+$type = $_GET['type'];
+$cap_id = $_GET['cap_id'];
+
+function callEndpoint($url) {
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, false);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  $curl_response = curl_exec($curl);
+  curl_close($curl);
+  return json_decode($curl_response);
+}
+
+$base_data = callEndpoint('https://admin.staffconnect-app.com/api/identify/' . $domain);
+
+$api_baseurl = 'https://api.' . $base_data->name . '.staffconnect-app.com/api';
+
+$card = callEndpoint($api_baseurl . '/user/' . $user_id . '/' . $type . '/' . $cap_id);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title><?php echo $card->meta->name . ' | Staffconnect4'; ?></title>
+  <base href="/">
+
+  <meta charset="utf-8">
+  <meta name="description" content="StaffConnect4 showcase for <?php echo $card->meta->name; ?>">
+  <meta name="keywords" content="StaffConnect,Staff management system,promotions,acting,modelling,scheduling,accounting,management">
+  <meta name="author" content="Nobox Limited">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <meta property="og:title" content="<?php echo $card->meta->name . ' | Staffconnect4'; ?>">
+  <meta property="og:description" content="StaffConnect4 showcase for <?php echo $card->meta->name; ?>">
+  <meta property="og:image" content="<?php echo $card->meta->thumbnail; ?>">
+  <meta property="og:url" content="https://<?php echo $domain; ?>/showcase.php?user_id=<?php echo $user_id; ?>&type=<?php echo $type; ?>&cap_id=<?php echo $cap_id; ?>">
+  <meta property="og:type" content="website">
+  <meta property="fb:app_id" content="558882467615844">
+
+  <style>
+    body {
+      margin: 0;
+    }
+  </style>
+
+  <script>
+    var data = <?php echo json_encode($card); ?>;
+    data.type = '<?php echo $type; ?>';
+  </script>
+</head>
+<body>
+  <div id="iframeWrapper"></div>
+  <script>
+    var iframeUrl = 'https://showcaseconnect.net/main/templates/view/' + data.other_id + '?public=true';
+    var iframeWrapper = document.getElementById('iframeWrapper');
+    var iframeEle = document.createElement('iframe');
+    iframeEle.src = iframeUrl;
+    iframeEle.frameBorder = 0;
+    iframeEle.width = window.innerWidth;
+    iframeEle.height = window.innerHeight;
+    iframeWrapper.appendChild(iframeEle);
+    iframeEle.onload = function () {
+      iframeEle.contentWindow.postMessage(data, "*");
+    }
+  </script>
+</body>
+</html>
